4311f0696a9d0058078bf2615e315ba7fb1f8784
johnsonoklii <johnsonoklii@163.com>
2025-02-16 12:32:13 +0800

    feature: multi function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 #include "div.h"
+#include "multi.h"
 
 int main() {
     int a = 10;
     int b = 2;
-    printf("a / b = %d\n", div(a, b));
+    printf("a * b = %d\n", multi(a, b));
 }
